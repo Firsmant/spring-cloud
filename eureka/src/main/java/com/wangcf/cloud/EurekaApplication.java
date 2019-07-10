b6f1bf5e227c3da99528c41920c07d651f146010
wangcf <171250669@qq.com>
2019-07-10 23:29:22 +0800

    创建一个Eureka Server

---
+++ eureka/src/main/java/com/wangcf/cloud/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.wangcf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: wangcf
+ * @Date: 2019/7/10 21:37
+ * @Description ${description}
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
