14c8ff8236f47eaa22436701c67a685d5a9a167b
wangcf <171250669@qq.com>
2019-07-12 00:12:24 +0800

    将movie注册到 Eureka Server

--- movie/src/main/java/com/wangcf/cloud/MovieApplication.java
+++ movie/src/main/java/com/wangcf/cloud/MovieApplication.java
@@ -2,6 +2,7 @@ package com.wangcf.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
  * @Description ${description}
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieApplication {
 
     public static void main(String[] args) {
