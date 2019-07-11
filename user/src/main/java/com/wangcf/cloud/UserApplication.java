74e3ac604c5416ed201b19161e2a36357a58f211
wangcf <171250669@qq.com>
2019-07-12 00:14:40 +0800

    将User 注册到Eureka Server; 并且注册到集群中

--- user/src/main/java/com/wangcf/cloud/UserApplication.java
+++ user/src/main/java/com/wangcf/cloud/UserApplication.java
@@ -2,6 +2,8 @@ package com.wangcf.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: wangcf
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description ${description}
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
